53faf79c75d241d4300606b082dd7c34521f92b6
Cheng-Chih-Hsin <125848354+Cheng-Chih-Hsin@users.noreply.github.com>
2023-10-27 22:53:18 +0800

    新增了滑鼠按下去變成十字件和IsCheked直接有按了還設定座標

--- test3/MainWindow.xaml.cs
+++ test3/MainWindow.xaml.cs
@@ -1,33 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace test3
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        string shapeType = "line";
+        Color strokeColor = Colors.Red;
+        Brush strokeBrussh;
+        int strokeThickness = 1;
+        Point start, dest;
         public MainWindow()
         {
             InitializeComponent();
+            strokeColorPicter.SelectedColor = strokeColor;
+            strokeBrussh = new SolidColorBrush(strokeColor);
         }
 
         private void ShapeButton_Click(object sender, RoutedEventArgs e)
         {
+            var targetRadioButton = sender as RadioButton;
+            shapeType = targetRadioButton.Tag.ToString();
+            //MessageBox.Show(shapeType);
+        }
 
+        private void myCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            dest = e.GetPosition(myCanvas);
+        }
+
+        private void myCanvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            start = e.GetPosition(myCanvas);
+
+            myCanvas.Cursor = Cursors.Cross;
+            switch (shapeType)
+            {
+                case "line":
+                    DrawLine(strokeColor , 1);
+                    break;
+                case "rectangle":
+                    break;
+                case "ellipse":
+                    break;
+            }
+        }
+
+        private void DrawLine(Color color, int thickness)
+        {
+            var brush = new SolidColorBrush(color);
+            Line line = new Line
+            {
+                Stroke = brush,
+                StrokeThickness = thickness,
+                X1 = start.X,
+                Y1 = start.Y,
+                X2 = dest.X,
+                Y2 = dest.Y
+            };
+            myCanvas.Children.Add(line);
         }
     }
 }
