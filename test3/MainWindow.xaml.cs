d376687b73bde8791bd64820c7dd34059bf599e6
Cheng-Chih-Hsin <125848354+Cheng-Chih-Hsin@users.noreply.github.com>
2023-10-29 15:15:12 +0800

    新增矩形和橢圓形還有清除畫布

--- test3/MainWindow.xaml.cs
+++ test3/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -15,14 +17,14 @@ namespace test3
     {
         string shapeType = "line";
         Color strokeColor = Colors.Red;
-        Brush strokeBrussh;
+        Color fillColor = Colors.Yellow;
         int strokeThickness = 1;
         Point start, dest;
         public MainWindow()
         {
             InitializeComponent();
             strokeColorPicter.SelectedColor = strokeColor;
-            strokeBrussh = new SolidColorBrush(strokeColor);
+            fillColorPicter.SelectedColor = fillColor;
         }
 
         private void ShapeButton_Click(object sender, RoutedEventArgs e)
@@ -34,18 +36,40 @@ namespace test3
 
         private void myCanvas_MouseMove(object sender, MouseEventArgs e)
         {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            { 
             dest = e.GetPosition(myCanvas);
             DisplayStatus();
+                Point origin;
+                origin.X = Math.Min(start.X, dest.X);
+                origin.Y = Math.Min(start.Y, dest.Y);
+                double width = Math.Abs(start.X - dest.X);
+                double height = Math.Abs(start.Y - dest.Y);
 
-            switch (shapeType)
+                switch (shapeType)
             {
-                case "line":                  
+                case "line":
+                    var line = myCanvas.Children.OfType<Line>().LastOrDefault();
+                    line.X2 = dest.X;
+                    line.Y2 = dest.Y;
                     break;
                 case "rectangle":
+                        var rect = myCanvas.Children.OfType<Rectangle>().LastOrDefault();
+                        rect.Width = width;
+                        rect.Height = height;
+                        rect.SetValue(Canvas.LeftProperty, origin.X);
+                        rect.SetValue(Canvas.TopProperty, origin.Y);
                     break;
                 case "ellipse":
-                    break;
+                        var ellipes = myCanvas.Children.OfType<Ellipse>().LastOrDefault();
+                        ellipes.Width = width;
+                        ellipes.Height = height;
+                        ellipes.SetValue(Canvas.LeftProperty, origin.X);
+                        ellipes.SetValue(Canvas.TopProperty, origin.Y);
+                        break;
+            }   
             }
+            
         }
 
         private void myCanvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -57,33 +81,94 @@ namespace test3
             switch (shapeType)
             {
                 case "line":
-                    DrawLine(strokeColor , 1);
+                    Line line = new Line 
+                    { 
+                        X1 = start.X,
+                        Y1 = start.Y,
+                        X2 = dest.X,
+                        Y2 = dest.Y,
+                        StrokeThickness = 1,
+                        Stroke = Brushes.Gray
+                    };
+                    myCanvas.Children.Add(line);
                     break;
                 case "rectangle":
+                    Rectangle rect = new Rectangle
+                    {
+                    Stroke = Brushes.Gray,
+                    Fill = Brushes.LightGray
+                    };
+                    myCanvas.Children.Add(rect);
+                    rect.SetValue(Canvas.LeftProperty, start.X);
+                    rect.SetValue(Canvas.TopProperty, start.Y);
                     break;
                 case "ellipse":
+                    Ellipse ellipes = new Ellipse
+                    {
+                        Stroke = Brushes.Gray,
+                        Fill = Brushes.LightGray
+                    };
+                    myCanvas.Children.Add(ellipes);
+                    ellipes.SetValue(Canvas.LeftProperty, start.X);
+                    ellipes.SetValue(Canvas.TopProperty, start.Y);
                     break;
             }
+        }
+
+        private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Brush strokeBrush = new SolidColorBrush(strokeColor);
+            Brush fillBrush = new SolidColorBrush(fillColor);
+            switch (shapeType)
+            {
+                case "line":
+                    var line = myCanvas.Children?.OfType<Line>().LastOrDefault();
+                    line.Stroke = strokeBrush;
+                    line.StrokeThickness = strokeThickness;
+                    break;
+                case "rectangle":
+                    var rect = myCanvas.Children.OfType<Rectangle>().LastOrDefault();
+                    rect.Stroke = new SolidColorBrush(strokeColor);
+                    rect.Fill = fillBrush;
+                    rect.StrokeThickness = strokeThickness;
+                    break;
+                case "ellipse":
+                    var ellipse = myCanvas.Children.OfType<Ellipse>().LastOrDefault();
+                    ellipse.Stroke = new SolidColorBrush(strokeColor);
+                    ellipse.Fill = fillBrush;
+                    ellipse.StrokeThickness = strokeThickness;
+                    break;
+            }
+        }
+
+        private void strokeColorPicter_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            strokeColor = (Color)strokeColorPicter.SelectedColor;
+        }
+
+        private void strokeThicknessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            strokeThickness = Convert.ToInt32(strokeThicknessSlider.Value);
+        }
+
+        private void fillColorPicter_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fillColor = (Color)fillColorPicter.SelectedColor;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myCanvas.Children.Clear();
+            DisplayStatus();
         }
 
         private void DisplayStatus()
         {
             coordinateLbel.Content = $"座標點:({Math.Round(start.X)},{Math.Round(start.Y)}) - ({dest.X},{dest.Y})";
-        }
-
-        private void DrawLine(Color color, int thickness)
-        {
-            var brush = new SolidColorBrush(color);
-            Line line = new Line
-            {
-                Stroke = brush,
-                StrokeThickness = thickness,
-                X1 = start.X,
-                Y1 = start.Y,
-                X2 = dest.X,
-                Y2 = dest.Y
-            };
-            myCanvas.Children.Add(line);
+            int lineCount = myCanvas.Children.OfType<Line>().Count();
+            int rectCount = myCanvas.Children.OfType<Rectangle>().Count();
+            int ellipseCount = myCanvas.Children.OfType<Ellipse>().Count();
+            shapeLabelLbel.Content = $"Line: {lineCount}, Rectangle{rectCount}, Ellipse{ellipseCount}";
         }
     }
 }
